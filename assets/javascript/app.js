38df1466de8d11cefbe86a38586d95c9afb7ac57
SallyTemple <pfende@gmail.com>
2018-03-05 17:34:39 -0700

    update4

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,30 +1,6 @@
 $(document).ready(function () {
 
-    // Create a timer Object:
 
-    var index = 0;
-    var oceanTriviaTimer = {
-        time: 25,
-        start: function () {
-            timeCounter = setInterval(oceanTriviaTimer.count, 1000);
-        },
-
-        stop: function () {
-            clearInterval(timeCounter)
-        },
-
-        reset: function () {
-            this.time = 25;
-            $("#timer").html("<h3>" + this.time + " seconds remaining </h3>");
-        },
-
-        count: function () {
-        if (oceanTriviaTimer.time > 0) {
-            $ ("#timer").html("<h3>" + ceanTriviaTimer.timne + " seconds remain</h3>");
-        }
-        }
-
-    };
     // Create an object for the questions
 
     var correct = 0;
@@ -63,5 +39,141 @@ $(document).ready(function () {
     };
 
     var triviaQuestions = ["question1", "question2", "question3", "question4", "question5"]
+    // Create a timer Object:
+
+    var index = 0;
+    var oceanTriviaTimer = {
+        time: 25,
+        start: function () {
+            timeCounter = setInterval(oceanTriviaTimer.count, 1000);
+        },
+
+        stop: function () {
+            clearInterval(timeCounter)
+        },
+
+        reset: function () {
+            this.time = 25;
+            $("#timer").html("<h3>" + this.time + " seconds remaining </h3>");
+        },
+
+        count: function () {
+            OceanTriviaTimer.time;
+            console.log(ceanTriviaTimer);
+            if (oceanTriviaTimer.time > 0) {
+                $("#timer").html("<h3>" + ceanTriviaTimer.timne + " seconds remain</h3>");
+            }
+            else {
+                index++;
+                incorrectAns();
+                OceanTriviaTimer.reset();
+            }
+
+            if (index < triviaQuestions.length) {
+                showQuestion(index);
+            }
+            else {
+                $(".options").hide();
+            }
+        }
+
+    };
+
+    function showQuestion(choices) {
+
+        oceanTriviaTimer.reset();
+        $("#question").html("<h3>" + triviaQuestions[choices].question + "</h3>");
+        $("#buttona").text(triviaQuestions[choices].ans[0, 1, 2]).show();
+        $("#buttonb").text(triviaQuestions[choices].ans[0, 1, 2]).show();
+        $("#buttonc").text(triviaQuestions[choices].and[0, 1, 2]).show();
+
+    }
+    function questionSetUp() {
+        index = 0;
+        $("#question").append("<button id='start'>Start</button>");
+        $("#start").on("click", function () {
+            $(this).hide();
+            oceanTriviaTimer.start();
+            showQuestion(index);
+        });
+    }
+
+    function getAnswer() {
+
+        $('.options').on('click', function() {
+          console.log('alert', index);
+            index++;
+            console.log('click', index);
+            $(".question").text('');
+            $("#buttona").text('');
+            $("#buttonb").text('');
+            $("#buttonc").text('');
+            showQuestion();
+        })
+    }
+    
+    function ansCorrect() {
+        correct++;
+        alert("correct!");
+        console.log("correct");
+    }
+    
+    function ansIncorrect() {
+        incorrect++;
+        alert("incorrect!");
+        console.log("incorrect");
+    }
+    
+    function showResult() {
+        $("#question").empty();
+        $("#question").append("<h3><p>" + correct + " correct1</p></h3>");
+        $("#question").append("<h3><p>" + incorrect + " incorrect1</p></h3>");
+        oceanTriviaTimer.stop();
+        $("#timer").empty();
+    
+    }
+    
+    setup();
+    $('.options').on('click', function() {
+     console.log($(this));
+     if(this.id == 'buttona') {
+         var multipleChoice = 'a';
+     } else if(this.id == 'buttonb') {
+         multipleChoice = 'b';
+     } else if (this.id == 'buttonc') {
+         multipleChoice = 'c';
+    
+     } 
+     if ((multipleChoice === 'a') && (triviaQuestions[index].flags[0] == true)) {
+         ansCorrect();
+     } else if (multipleChoice === 'a') {
+         ansIncorrect();
+     }
+     if ((multipleChoice === 'b') && (triviaQuestions[index].flags[1] == true)) {
+         ansCorrect();
+     } else if (multipleChoice === 'b') {
+         ansIncorrect();
+     }
+    if ((multipleChoice === 'c') && (triviaQuestions[index].flags[2] == true)) {
+         ansCorrect();
+     } else if (multipleChoice === 'c') {
+         ansIncorrect();
+     }
+   
+    
+     $("#question").text('');
+     $("#buttona").text('');
+     $("#buttonb").text('');
+     $("#buttonc").text('');
+     
+     index++;
+     if (index < triviaQuestions.length) {
+         showQuestion(index);
+     } else {
+         $(".options").hide();
+         showResult();
+     }
+    });
+    
 
 });
